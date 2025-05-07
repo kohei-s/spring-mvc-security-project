495639d63f40f91ec3219a49267352b3be7cacd8
kohei-s <koheisuzuki@me.com>
2025-05-07 15:40:35 +0200

    project set up

---
+++ src/main/java/com/koheis/springmvcsecurityproject/SpringMvcSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.koheis.springmvcsecurityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSecurityProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koheis/springmvcsecurityproject/SpringMvcSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koheis.springmvcsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
